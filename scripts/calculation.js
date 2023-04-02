1b7e2017944684ce011b4f71984f0aa07a6a8f50
nurealammiaji <nurealam.miaji@gmail.com>
2023-04-02 17:38:20 +0600

    case calculation added

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -1,3 +1,5 @@
+// iPhone Calculation
+
 function iPhonePlusMinus(condition) {
     let iphoneQuantity = document.getElementById('iphone-quantity');
     let iphoneQuantityValue = iphoneQuantity.value;
@@ -9,5 +11,21 @@ function iPhonePlusMinus(condition) {
     else {
         let iphoneQuantityMinus = iphoneQuantityValueNumeric - 1;
         iphoneQuantity.value = iphoneQuantityMinus;
+    }
+}
+
+// iPhone Case Calculation
+
+function iPhoneCasePlusMinus(condition) {
+    let iphoneCaseQuantity = document.getElementById('iphone-case-quantity');
+    let iphoneCaseQuantityValue = iphoneCaseQuantity.value;
+    let iphoneCaseQuantityValueNumeric = parseInt(iphoneCaseQuantityValue);
+    if(condition === true) {
+        let iphoneCaseQuantityPlus = iphoneCaseQuantityValueNumeric + 1;
+        iphoneCaseQuantity.value = iphoneCaseQuantityPlus;
+    }
+    else {
+        let iphoneCaseQuantityMinus = iphoneCaseQuantityValueNumeric - 1;
+        iphoneCaseQuantity.value = iphoneCaseQuantityMinus;
     }
 }
