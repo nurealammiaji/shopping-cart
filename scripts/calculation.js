61c07bab3f167d879bdbba80964b2cb937a632a4
nurealammiaji <nurealam.miaji@gmail.com>
2023-04-02 19:50:43 +0600

    linked price with quantity

--- scripts/calculation.js
+++ scripts/calculation.js
@@ -1,23 +1,41 @@
 // iPhone Calculation
+
 function iPhonePlusMinus(condition) {
+
     let iphoneQuantity = document.getElementById('iphone-quantity');
     let iphoneQuantityValue = iphoneQuantity.value;
     let iphoneQuantityValueNumeric = parseInt(iphoneQuantityValue);
+
+    let iphonePrice = document.getElementById('iphone-price');
+    let iphonePriceString = iphonePrice.innerText;
+    let iphonePriceNumeric = parseInt(iphonePriceString);
+
     if(condition === true) {
         let iphoneQuantityPlus = iphoneQuantityValueNumeric + 1;
         iphoneQuantity.value = iphoneQuantityPlus;
+
+        let iphonePricePlus = 1219 * iphoneQuantityPlus;
+        iphonePrice.innerText = iphonePricePlus;
+
     }
     else {
         let iphoneQuantityMinus = iphoneQuantityValueNumeric - 1;
         iphoneQuantity.value = iphoneQuantityMinus;
+
+        let iphonePriceMinus = 1219 * iphoneQuantityMinus;
+        iphonePrice.innerText = iphonePriceMinus;
     }
 }
 
+
 // iPhoneCase Calculation
+
 function iPhoneCasePlusMinus(condition) {
+
     let iphoneCaseQuantity = document.getElementById('iphone-case-quantity');
     let iphoneCaseQuantityValue = iphoneCaseQuantity.value;
     let iphoneCaseQuantityValueNumeric = parseInt(iphoneCaseQuantityValue);
+
     if(condition === true) {
         let iphoneCaseQuantityPlus = iphoneCaseQuantityValueNumeric + 1;
         iphoneCaseQuantity.value = iphoneCaseQuantityPlus;
@@ -47,3 +65,4 @@ document.getElementById('iphone-case-quantity-plus').addEventListener('click', f
 document.getElementById('iphone-case-quantity-minus').addEventListener('click', function(){
     iPhoneCasePlusMinus(false);
 })
+
